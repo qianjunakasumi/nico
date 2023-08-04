2de62778388ac1f771447782bcda3118f1c3df85
千橘 雫霞 <i@qianjunakasumi.moe>
2023-08-04 16:58:30 +0800

    chore:sparkles:database declare setup func.

---
+++ internal/database/setup.go
@@ -0,0 +1,14 @@
+// Copyright (c) 2023 qianjunakasumi (https://github.com/qianjunakasumi)
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+package database
+
+// initMongoConnection
+func initMongoConnection() {
+	// todo read mongodb config and connect to it, timeout: 8s
+}
+
+// todo init mongo collections and documents (need data model)
