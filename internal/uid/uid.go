e6cb4e85203a79522ccab95b121655697d6ce36f
千橘 雫霞 <i@qianjunakasumi.moe>
2023-08-06 11:01:56 +0800

    feat:sparkles:uid abstract id generator that implementations can be replaced

---
+++ internal/uid/uid.go
@@ -0,0 +1,21 @@
+// Copyright (c) 2023 qianjunakasumi (https://github.com/qianjunakasumi)
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+package uid
+
+// UID is used for most identification codes of nico,
+// including url address conversion (core component)
+type UID int64
+
+// IUIDService provide abstraction of global number allocator,
+// it's around to generate an UID
+type IUIDService interface {
+	// NewProvider return an IUIDService instance which actually serve nico
+	NewProvider() IUIDService
+
+	// Generate an UID
+	Generate() (UID, error)
+}
