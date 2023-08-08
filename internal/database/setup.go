15d2f99feadcc48202a461fda8717ed5a5094d2d
XXieYiqiang <136306330+XXieYiqiang@users.noreply.github.com>
2023-08-08 14:50:49 +0800

    feat:sparkles:database implement mongodb connect and dis~ func. (#1)
    * feat:sparkles:database initialize mongodb
    
    Signed-off-by: XieYiqiang <1103938364@qq.com>
    
    * feat:sparkles:database make external context and disconnection func.
    
    ---------
    
    Signed-off-by: XieYiqiang <1103938364@qq.com>
    Co-authored-by: 千橘 雫霞 <i@qianjunakasumi.moe>

--- internal/database/setup.go
+++ internal/database/setup.go
@@ -6,9 +6,41 @@
 
 package database
 
-// initMongoConnection
-func initMongoConnection() {
-	// todo read mongodb config and connect to it, timeout: 8s
+import (
+	"context"
+
+	"github.com/rs/zerolog/log"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+// InitMongoConnection returns a mongo client and database connection
+func InitMongoConnection(ctx context.Context, url string) (client *mongo.Client, database *mongo.Database, err error) {
+	log.Debug().Str("target", url).Msg("connecting mongodb")
+
+	client, err = mongo.Connect(
+		ctx,
+		options.Client().ApplyURI(url),
+	)
+	if err != nil {
+		return
+	}
+
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		return
+	}
+
+	database = client.Database("senjunico")
+	log.Debug().Msg("connected mongodb and apply database to senjunico")
+
+	return
+}
+
+// DisconnectMongo disconnects the mongo client
+func DisconnectMongo(ctx context.Context, mongo *mongo.Client) error {
+	return mongo.Disconnect(ctx)
 }
 
 // todo init mongo collections and documents (need data model)
